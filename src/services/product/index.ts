edba2692030565240e7774dc0d453f5f1ea185ec
Joao Pedro Villas Boas <joaopedrombvboas@gmail.com>
2023-03-27 17:49:10 -0300

    feat: Remove useless console log

--- src/services/product/index.ts
+++ src/services/product/index.ts
@@ -13,7 +13,7 @@ const DAYS_IN_HOURS = 1000 * 3600;
 export const getProduct = async (product: ProductRequest) => {
   const url = getDomainFromURL(product.url);
   verifyAllowedUrl(url);
-  console.log(url);
+
   const existentProduct = await getProductOnDatabase(product.url);
 
   if (existentProduct && isProductUpdated(existentProduct)) {
